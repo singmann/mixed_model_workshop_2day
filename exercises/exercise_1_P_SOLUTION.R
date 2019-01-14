fa26b00b04cbd675dd626d4284ea1a424eec6a8c
Henrik Singmann <singmann@gmail.com>
2019-01-14 14:02:12 +0000

    part 1 basically done

---
+++ exercises/exercise_1_P_SOLUTION.R
@@ -0,0 +1,102 @@
+## ---- message=FALSE, warning=FALSE---------------------------------------
+library("tidyverse")
+library("broom") # not automatically loaded
+
+## ------------------------------------------------------------------------
+# Run complete chunk: Ctrl+Shift+Enter
+
+# You might need to set the correct working directory via the menu: 
+# Session -> Set Working Directory -> To Source File Location
+
+afex::set_sum_contrasts() # just in case we set orthogonal contrasts
+
+load("ssk16_dat_prepared_ex1.rda") # data preapred in 'prepare_data.R'
+glimpse(dat1)
+
+
+## ------------------------------------------------------------------------
+m0 <- lm(if_A_then_B ~ B_given_A, dat1)
+summary(m0)
+
+## ---- fig.width=7, fig.height=3------------------------------------------
+ggplot(data = dat1) + 
+  geom_point(mapping = aes(x = B_given_A, y = if_A_then_B), alpha = 0.2, pch = 16) + 
+  coord_fixed()
+
+## ------------------------------------------------------------------------
+
+ex1_no_pooling_estimates <- dat1 %>% 
+  group_by(p_id) %>% 
+  do(tidy(lm(if_A_then_B ~ B_given_A, .)))
+head(ex1_no_pooling_estimates)
+
+
+## ------------------------------------------------------------------------
+ex1_slopes <- ex1_no_pooling_estimates %>% 
+  filter(term == "B_given_A")
+
+ggplot(ex1_slopes, aes(estimate)) +
+  geom_histogram(bins = 30) 
+
+## ------------------------------------------------------------------------
+summary(lm(estimate ~ 1, ex1_slopes))
+
+## ------------------------------------------------------------------------
+load("ssk16_dat_prepared_ex2.rda")
+str(dat2)
+
+## ------------------------------------------------------------------------
+afex::set_sum_contrasts()
+library("emmeans")
+
+ex2_comp_1 <- lm(if_A_then_B ~ B_given_A * rel_cond, dat2)
+car::Anova(ex2_comp_1, type = 3)
+
+emtrends(ex2_comp_1, "rel_cond", var = "B_given_A")
+
+
+## ------------------------------------------------------------------------
+
+ex2_comp_2 <- dat2 %>% 
+  group_by(rel_cond, p_id) %>% 
+  summarise_if(is.numeric, mean) %>% 
+  lm(if_A_then_B ~ B_given_A * rel_cond, .)
+car::Anova(ex2_comp_2, type = 3)
+
+emtrends(ex2_comp_2, "rel_cond", var = "B_given_A")
+
+
+## ------------------------------------------------------------------------
+
+ex2_comp_3 <- dat2 %>% 
+  group_by(rel_cond, i_id) %>% 
+  summarise_if(is.numeric, mean) %>% 
+  lm(if_A_then_B ~ B_given_A * rel_cond, .)
+car::Anova(ex2_comp_3, type = 3)
+
+emtrends(ex2_comp_3, "rel_cond", var = "B_given_A")
+
+
+## ------------------------------------------------------------------------
+ex2_no_pooling_estimates <- dat2 %>% 
+  group_by(p_id, rel_cond) %>% 
+  do(tidy(lm(if_A_then_B ~ B_given_A, .)))
+head(ex2_no_pooling_estimates)
+
+## ------------------------------------------------------------------------
+ex2_slopes <- ex2_no_pooling_estimates %>% 
+  filter(term == "B_given_A")
+
+ggplot(ex2_slopes, aes(estimate)) +
+  geom_histogram(bins = 30) +
+  facet_wrap(~ rel_cond)
+
+## ---- message=FALSE------------------------------------------------------
+library("afex")
+
+(a1 <- aov_car(estimate ~ rel_cond + Error(p_id/rel_cond), ex2_slopes))
+
+
+## ------------------------------------------------------------------------
+emmeans::emmeans(a1, "rel_cond")
+
